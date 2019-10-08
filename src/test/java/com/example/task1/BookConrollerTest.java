e6dd064b162a05f0f648f90e186be19e01e09a4e
RUstam163 <minibaev-rustam163@yandex.ru>
2019-10-08 14:23:17 +0400

    Added BookControllerTest with MockMVC for home page

---
+++ src/test/java/com/example/task1/BookConrollerTest.java
@@ -0,0 +1,39 @@
+package com.example.task1;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import com.example.task1.controller.BookController;
+import com.example.task1.service.BookService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(BookController.class)
+public class BookConrollerTest {
+
+    @MockBean
+    private BookService bookService;
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"))    /*Ожидает home*/
+                .andExpect(content().string( /*Welcome to...*/
+
+                        containsString("Книги")));
+    }
+}
+
+
